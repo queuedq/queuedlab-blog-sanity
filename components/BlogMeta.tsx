c19ab59a24dd58730531bc8dd5be5a04a204a03f
Han Dongkyu <queuedq@gmail.com>
2023-04-24 11:26:20 +0900

    Update theme color

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -25,9 +25,9 @@ export default function BlogMeta() {
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#ffffff" />
     </>
   )
 }
