4fec9d6e82f1ff8629845ca818f41806ee52c153
Han Dongkyu <queuedq@gmail.com>
2023-04-24 12:28:15 +0900

    Refactor

--- components/pages/IndexPage.tsx
+++ components/pages/IndexPage.tsx
@@ -1,3 +1,4 @@
+import { toPlainText } from '@portabletext/react'
 import BlogHeader from 'components/layout/BlogHeader'
 import Layout from 'components/layout/BlogLayout'
 import PostList from 'components/post/PostList'
@@ -5,7 +6,40 @@ import * as demo from 'lib/demo.data'
 import type { Post, Settings } from 'lib/sanity.queries'
 import Head from 'next/head'
 
-import IndexPageHead from './IndexPageHead'
+import BlogMeta from './BlogMeta'
+
+
+interface IndexPageHeadProps {
+  settings: Settings
+}
+
+function IndexPageHead({ settings }: IndexPageHeadProps) {
+  const {
+    title = demo.title,
+    description = demo.description,
+    ogImage = {},
+  } = settings
+  const ogImageTitle = ogImage?.title || demo.ogImageTitle
+
+  return (
+    <>
+      <title>{title}</title>
+      <BlogMeta />
+      <meta key="description" name="description" content={toPlainText(description)} />
+      <meta
+        property="og:image"
+        // Because OG images must have a absolute URL, we use the
+        // `VERCEL_URL` environment variable to get the deployment’s URL.
+        // More info:
+        // https://vercel.com/docs/concepts/projects/environment-variables
+        content={`${
+          process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
+        }/api/og?${new URLSearchParams({ title: ogImageTitle })}`}
+      />
+    </>
+  )
+}
+
 
 export interface IndexPageProps {
   preview?: boolean
--- components/pages/PostPage.tsx
+++ components/pages/PostPage.tsx
@@ -4,11 +4,39 @@ import PostBody from 'components/post/PostBody'
 import PostHeader from 'components/post/PostHeader'
 import PostList from 'components/post/PostList'
 import * as demo from 'lib/demo.data'
+import { urlForImage } from 'lib/sanity.image'
 import type { Post, Settings } from 'lib/sanity.queries'
 import Head from 'next/head'
 import { notFound } from 'next/navigation'
 
-import PostPageHead from './PostPageHead'
+import BlogMeta from './BlogMeta'
+
+
+interface PostPageHeadProps {
+  settings: Settings
+  post: Post
+}
+
+function PostPageHead({ settings, post }: PostPageHeadProps) {
+  const title = settings.title ?? demo.title
+  return (
+    <>
+      <title>{post.title ? `${post.title} | ${title}` : title}</title>
+      <BlogMeta />
+      {post.coverImage?.asset?._ref && (
+        <meta
+          property="og:image"
+          content={urlForImage(post.coverImage)
+            .width(1200)
+            .height(627)
+            .fit('crop')
+            .url()}
+        />
+      )}
+    </>
+  )
+}
+
 
 export interface PostPageProps {
   preview?: boolean
