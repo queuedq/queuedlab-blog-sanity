a869e2a81ea19e58d11de51b341812dca55a09db
Han Dongkyu <queuedq@gmail.com>
2023-05-01 14:32:04 +0900

    Use Pretendard variable dynamic subset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import 'public/fonts/pretendard/pretendardvariable-dynamic-subset.css'
 
 import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="bg-white font-sans text-gray-700">
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,8 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin=""
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css"
-        />
-      </Head>
-      <body className="bg-white text-gray-700">
+      <Head></Head>
+      <body>
         <Main />
         <NextScript />
       </body>
