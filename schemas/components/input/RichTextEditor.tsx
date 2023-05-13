6da9f1fd7d92313595e435a4f64c88f3e5d97a2d
Han Dongkyu <queuedq@gmail.com>
2023-05-13 17:39:33 +0900

    Handle non-markdown paste also

--- schemas/components/input/RichTextEditor.tsx
+++ schemas/components/input/RichTextEditor.tsx
@@ -1,11 +1,9 @@
-import { normalizeBlock } from '@sanity/block-tools'
 import { PortableTextEditor } from '@sanity/portable-text-editor'
 import { Inline, Stack, Switch, Text } from '@sanity/ui'
 import { useState } from 'react'
 import { BlockEditor } from 'sanity'
 
-import { handleMarkdownPaste } from './handleMarkdownPaste'
-import { htmlPastePatch } from './htmlParser'
+import { handlePaste } from './handlePaste'
 
 export function RichTextEditor(props) {
   const [markdownPaste, setMarkdownPaste] = useState(false)
@@ -14,7 +12,7 @@ export function RichTextEditor(props) {
     <Stack space={3}>
       <BlockEditor
         {...props}
-        onPaste={markdownPaste ? handleMarkdownPaste : handlePaste}
+        onPaste={handlePaste(markdownPaste)}
         hotkeys={{
           custom: {
             'mod+opt+1': (event, editor) => {
@@ -46,44 +44,9 @@ export function RichTextEditor(props) {
           label={`Markdown paste (${markdownPaste ? 'on' : 'off'})`}
           onChange={() => setMarkdownPaste(!markdownPaste)}
           checked={markdownPaste}
-          />
+        />
         <Text>Markdown paste</Text>
       </Inline>
     </Stack>
   )
-}
-
-function handlePaste(input) {
-  return undefined
-
-  // https://www.sanity.io/docs/customizing-the-portable-text-editor
-  const { event, schemaTypes, path } = input
-  const text = event.clipboardData.getData('text')
-  const html = event.clipboardData.getData('text/html')
-  const json = event.clipboardData.getData('application/json')
-  const pt = event.clipboardData.getData('application/x-portable-text')
-  // console.log(text)
-  // console.log(html)
-  // console.log(json)
-  // console.log(pt)
-
-  if (!html) {
-    // This code deals with Sanity bug that removes contiguous whitespaces and line breaks...
-    // ref: https://github.com/sanity-io/sanity/issues/1814#issuecomment-858799468
-    // It seems that Sanity removes contiguous whitespaces in htmlToBlock().
-    // https://github.com/sanity-io/sanity/issues/3977
-    return {
-      insert: [
-        // TODO: fix type issue (probably sanity is responsible for it)
-        normalizeBlock({
-          _type: 'block',
-          // @ts-ignore
-          children: [{ _type: 'span', text }],
-        }),
-      ],
-      path,
-    }
-  }
-
-  return htmlPastePatch(html, schemaTypes, path)
 }
---
+++ schemas/components/input/handlePaste.ts
@@ -0,0 +1,58 @@
+import { normalizeBlock, TypedObject } from '@sanity/block-tools'
+import { OnPasteFn } from '@sanity/portable-text-editor'
+import remarkHtml from 'remark-html'
+import remarkMath from 'remark-math'
+import remarkParse from 'remark-parse'
+import { unified } from 'unified'
+
+import { htmlPastePatch } from './htmlParser'
+
+export const handlePaste =
+  (isMarkdownPaste): OnPasteFn =>
+  async (input) => {
+    // https://www.sanity.io/docs/customizing-the-portable-text-editor
+    const { event, schemaTypes, path } = input
+    const text = event.clipboardData.getData('text')
+    const html = event.clipboardData.getData('text/html')
+    const json = event.clipboardData.getData('application/json')
+    const pt = event.clipboardData.getData('application/x-portable-text')
+
+    // console.log(text)
+    // console.log(html)
+    // console.log(json)
+    // console.log(pt)
+
+    if (pt) return undefined
+
+    if (isMarkdownPaste) {
+      const { value: html } = await markdownToHtml(text)
+      return htmlPastePatch(html, schemaTypes, path)
+    }
+
+    if (html) return htmlPastePatch(html, schemaTypes, path)
+
+    // This code deals with Sanity bug that removes contiguous whitespaces and line breaks...
+    // ref: https://github.com/sanity-io/sanity/issues/1814#issuecomment-858799468
+    // It seems that Sanity removes contiguous whitespaces in htmlToBlock().
+    // https://github.com/sanity-io/sanity/issues/3977
+    return {
+      insert: [
+        normalizeBlock({
+          _type: 'block',
+          children: [{ _type: 'span', text }],
+        } as TypedObject),
+      ],
+      path,
+    }
+  }
+
+async function markdownToHtml(markdownContent: string) {
+  // Plugin usage example (remark-math)
+  // https://github.com/remarkjs/remark-math/tree/main
+  const PT = await unified()
+    .use(remarkParse)
+    .use(remarkMath)
+    .use(remarkHtml, { sanitize: false })
+    .process(markdownContent)
+  return PT
+}
