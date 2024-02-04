20f57ba4ac002023c1f39492efc09e908826094c
Han Dongkyu <queuedq@gmail.com>
2024-02-05 03:45:39 +0900

    Better LaTeX CSS for mobile (full width scroll container)

--- components/rich-text/Latex.tsx
+++ components/rich-text/Latex.tsx
@@ -7,7 +7,14 @@ export default function Latex({ value, isInline }) {
   return isInline ? (
     <InlineMath>{value.body}</InlineMath>
   ) : (
-    <BlockMath>{value.body}</BlockMath>
+    // overflow-*   : For horizontal scroll
+    //                (See https://katex.org/docs/issues.html#css-customization)
+    // -mx-5 px-5   : For full width container in mobile
+    // py-1         : To provide natural margin
+    // For scroll margin at end, see katex-tweaks.css
+    <div className="my-6 py-1 -mx-5 px-5 overflow-x-auto overflow-y-hidden">
+      <BlockMath>{value.body}</BlockMath>
+    </div>
   )
 }
 
