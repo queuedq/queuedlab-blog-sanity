f75778f2c824755c7598889d569c76244a027830
Han Dongkyu <queuedq@gmail.com>
2024-02-20 02:06:26 +0900

    Letterbox tall images

--- components/rich-text/Figure.tsx
+++ components/rich-text/Figure.tsx
@@ -7,10 +7,14 @@ import { useNextSanityImage } from '@/sanity/lib/utils'
 export default function Figure({ value }) {
   const imageProps = useNextSanityImage(value.image)
 
-  // TODO: set size
   return (
     <figure>
-      <Image {...imageProps} alt={value.alt ?? ''} className="mx-auto" />
+      {/* max height for letterboxing tall images */}
+      <Image
+        {...imageProps}
+        alt={value.alt ?? ''}
+        className="max-h-[min(60vh,28em)] w-auto mx-auto"
+      />
       <figcaption>{value.caption}</figcaption>
     </figure>
   )
