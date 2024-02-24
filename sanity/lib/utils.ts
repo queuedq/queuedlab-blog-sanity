4439f7a205f45cf60e0624f8aa2491195b1fccd3
Han Dongkyu <queuedq@gmail.com>
2024-02-25 02:40:03 +0900

    Work around the cropping of vertical iPhone photos

--- sanity/lib/utils.ts
+++ sanity/lib/utils.ts
@@ -12,5 +12,24 @@ export const urlForImage = (source: any) =>
 
 // https://www.sanity.io/plugins/next-sanity-image
 export const useNextSanityImage = (image: SanityImageObject) => {
-  return _useNextSanityImage(client, image)
+  return _useNextSanityImage(client, image, {
+    imageBuilder: customImageBuilder,
+  })
+}
+
+// This function is to work around the cropping of vertical photos taken with iPhone.
+// The bug seems to be related to the EXIF orientation tag.
+// Related issue: https://github.com/sanity-io/sanity/issues/4036
+//
+// The workaround is to use `fit=max` instead of next-sanity-image's default `fit=clip`.
+// Docs on `fit`: https://www.sanity.io/docs/image-urls#fit-45b29dc6f09f
+// Using custom image builder for next-sanity-image:
+// https://www.sanity.io/plugins/next-sanity-image#image-transformations
+const customImageBuilder = (imageUrlBuilder, options) => {
+  return imageUrlBuilder
+    .width(
+      options.width || Math.min(options.originalImageDimensions.width, 1920),
+    )
+    .quality(options.quality || 75)
+    .fit('max')
 }
