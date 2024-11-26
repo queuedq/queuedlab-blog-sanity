d9fb427829b3cf17058461d62bd8fd75f3d2a216
Han Dongkyu <queuedq@gmail.com>
2024-11-26 22:30:10 +0900

    Disable footer if empty

--- components/post/PostFooter.tsx
+++ components/post/PostFooter.tsx
@@ -9,19 +9,24 @@ interface PostFooterProps {
 export default function PostFooter({ post }: PostFooterProps) {
   const { tags } = post
 
+  // Disable footer if empty
+  // Without this, there will be an unwanted margin because
+  // margin collapsing does not work with flexbox.
+  // (It's a workaround because I don't have a better idea now)
+  if (!tags) return null
+
   return (
     <div className="mt-16 flex flex-wrap gap-x-4 gap-y-1 leading-relaxed">
-      {tags &&
-        tags.map((tag) => (
-          <Link
-            key={tag.slug}
-            href={`/tags/${tag.slug}`}
-            className="border-link text-link hover:underline"
-          >
-            <span className="tracking-widest">#</span>
-            {tag.name}
-          </Link>
-        ))}
+      {tags.map((tag) => (
+        <Link
+          key={tag.slug}
+          href={`/tags/${tag.slug}`}
+          className="border-link text-link hover:underline"
+        >
+          <span className="tracking-widest">#</span>
+          {tag.name}
+        </Link>
+      ))}
     </div>
   )
 }
